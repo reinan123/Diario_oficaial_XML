494c33aaa0c20e6e459c6730300cde5253b13e36
reinan123 <44839887+reinan123@users.noreply.github.com>
2020-03-13 15:43:05 -0300

    Update extractDataXML.py

--- extractDataXML.py
+++ extractDataXML.py
@@ -1,6 +1,8 @@
 import xml.etree.ElementTree as ET
-import pandas as pd
+import csv
 import os
+
+informacoes = []
 
 
 def obter_arquivos_xml(diretorio):
@@ -10,56 +12,50 @@ def obter_arquivos_xml(diretorio):
         if arq.endswith('.xml'):
             ret.append(os.path.join(diretorio, arq))
 
+
 def arqsXML():
-    identificacao = minfra = secao = tipo_documento = data = publciacao =None
-    listaDic = []
-    dic = {
-        'orgao': minfra,
-        'secao': secao,
-        'tipo_documento': tipo_documento,
-        'texto': publciacao,
-        'identificacao': identificacao,
-        'data': data
-    }
     i = 0
     while i < len(ret):
         doc = ET.parse(ret[i])
         root = doc.getroot()
 
         for categ in root:
-            tipo_documento = categ.attrib['artType']
-            secao = categ.attrib['pubName']
-            data = categ.attrib['pubDate']
             orgao = categ.attrib['artCategory']
 
-            dic['secao'] = secao
-            dic['tipo_documento'] = tipo_documento
-            dic['data'] = data
-
             if 'Ministério da Infraestrutura/' in orgao:
-                Minfra = orgao
-                dic['orgao'] = orgao
+                minfra = orgao
+                tipo_documento = categ.attrib['artType']
+                secao = categ.attrib['pubName']
+                data = categ.attrib['pubDate']
 
                 for identifica in root.iter('Identifica'):
                     identificacao = identifica.text
-                    dic['identificacao'] = identificacao
 
                 for info in root.iter('Texto'):
                     texto = info.text
                     publciacao = texto.replace('<p class="identifica">', ' ').replace('</p><p class="ementa">', ' ').replace('</p><p>', ' ').replace('</p><p class="assina">', ' ').replace('</p><p class="cargo">', ' - ')[:-4]
-                    dic['texto'] = publciacao
 
-
-        csvXml = pd.DataFrame({'Orgão': [minfra], 'Identificação': [identificacao],
-                               'Tipo Documento ': [tipo_documento], 'Seção': [secao],
-                               'Data': [data], 'Texto': [publciacao]})
-
-        listaDic.append(dic)
-
-        df = pd.DataFrame(csvXml, columns=['Orgão', 'Identificação', 'Tipo Documento', 'Seção', 'Data', 'Texto'])
-        df.to_csv('DouSecao1.csv', sep=';', encoding='utf-8')
-        print(df)
+                    informacoes.append(minfra)
+                    informacoes.append(identificacao)
+                    informacoes.append(tipo_documento)
+                    informacoes.append(secao)
+                    informacoes.append(data)
+                    informacoes.append(publciacao)
         i += 1
 
-obter_arquivos_xml('C:\\Users\\reinan.oliveira\\Downloads\\2020-02-28-DO1')
+
+obter_arquivos_xml('caminho da pasta')
 arqsXML()
+
+n = 0
+while n < len(informacoes):
+
+    csv.register_dialect('myDialect', delimiter=';')
+    with open('DouSecao1.csv', 'w', newline='', encoding='windows-1252', errors='ignore') as saida:  # iso-8859-1  windows-1252  utf-8
+        escrever = csv.writer(saida, dialect='myDialect')
+        escrever.writerow(['Orgão', 'Identificação', 'Tipo Documento', 'Seção', 'Data', 'Texto'])
+        while n < len(informacoes):
+            escrever.writerow(
+                [informacoes[n], informacoes[n + 1], informacoes[n + 2], informacoes[n + 3], informacoes[n + 4], informacoes[n + 5]])
+            n += 6
+    n += 6
